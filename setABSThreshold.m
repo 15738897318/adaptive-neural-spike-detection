50423d74e08b322100013b8e8f5a40fae5c82f61
Dirk Brink <dirk.judd@live.com>
2018-05-07 10:40:52 +0100

    Static evaluation entrypoint function

--- setABSThreshold.m
+++ setABSThreshold.m
@@ -1,4 +1,4 @@
-function threshold = setABSThreshold(dataset, thresholdConstant)
-    standardDev = median(dataset./0.6745);
+function threshold = setABSThreshold(dataset, ~, thresholdConstant)
+    standardDev = abs(median(dataset/0.6745));
     threshold = thresholdConstant * standardDev;
 end
---
+++ staticEvaluation.m
@@ -0,0 +1,12 @@
+function staticEvaluation(datasetPath, tuningPercent, staticAlgo, thresholdConstant)
+    dataset = load(datasetPath);
+    data = dataset.data;
+    tuningNumber = round(size(data,2)*(tuningPercent/100));
+    tuningData = data(1:tuningNumber);
+    delta = 1;
+    size(tuningData);
+    threshold = staticAlgo(tuningData, delta, thresholdConstant);
+    plot(tuningData);
+    hold on;
+    plot(threshold.*ones(1,tuningNumber));
+end
