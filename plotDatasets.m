f18d8c931e33df0569b30202d7bdac3abca3c60f
Dirk Brink <dirk.judd@live.com>
2018-05-10 15:35:35 +0100

    Edited dataset plotting

--- plotDatasets.m
+++ plotDatasets.m
@@ -7,22 +7,17 @@ function plotDatasets()
     plot3 = plot3.data;
     plot4 = load('datasets/simulation_4.mat');
     plot4 = plot4.data;
-    plot5 = load('datasets/simulation_5.mat');
-    plot5 = plot5.data;
     
-    subplot(5,1,1);
-    plot(plot1(1:50000));
+    subplot(4,1,1);
+    plot(plot1(1:1440000));
     
-    subplot(5,1,2);
-    plot(plot2(1:50000));
+    subplot(4,1,2);
+    plot(plot2(1:1440000));
     
-    subplot(5,1,3);
-    plot(plot3(1:50000));
+    subplot(4,1,3);
+    plot(plot3(1:1440000));
     
-    subplot(5,1,4);
-    plot(plot4(1:50000));
-    
-    subplot(5,1,5);
-    plot(plot5(1:50000));
+    subplot(4,1,4);
+    plot(plot4(1:1440000));
     
 end
